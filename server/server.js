30ccfa13a8bb25d1343903fc3146895d069e30ae
syam <syamvishnu@gmail.com>
2024-04-16 10:50:19 +0530

    User Registration Both client and server completed

--- server/server.js
+++ server/server.js
@@ -4,17 +4,21 @@ import userRoutes from "./routes/userRoutes.js";
 import { errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 connectDB();
 dotenv.config();
 const app = Express();
 const port = process.env.PORT || 5000;
+app.use(cors());
 
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/api/users", userRoutes);
+
+
+app.use("/", userRoutes);
 
 // app.get("/", (req, res) => res.send("Server Is Ready"));
 
